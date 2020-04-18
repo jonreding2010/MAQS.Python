2710abeebfdd24ea46aa8e2e42ee17d8036c97f7
Jon Reding <jonr@magenic.com>
2020-04-18 00:58:55 -0500

    Add files via upload

---
+++ Framework/utilities/code/helper/Config.py
@@ -0,0 +1,12 @@
+import c
+
+# Configuration class.
+class Config:
+# The default section MagenicMaqs.
+    DEFAULT_MAQS_SECTION = ConfigSection.MagenicMaqs
+    
+# efault config.xml file name.
+    CONFIG_FILE = "config.xml"
+    
+    
+    
