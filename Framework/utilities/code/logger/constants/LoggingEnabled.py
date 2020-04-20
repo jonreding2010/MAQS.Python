cebe31ae5c16a3e2cac56f1d1d3d0bfa6c6d325f
Jon Reding <jonr@magenic.com>
2020-04-19 22:52:55 -0500

    Create LoggingEnabled.py

---
+++ Framework/utilities/code/logger/constants/LoggingEnabled.py
@@ -0,0 +1,12 @@
+from enum import Enum
+
+# The type of message.
+class LoggingEnabled(Enum):
+  # Yes log.
+  YES = "YES"
+
+  # Only save a log when there is a failure.
+  ONFAIL = "ONFAIL"
+
+  # No, don't log.
+  NO = "NO"
